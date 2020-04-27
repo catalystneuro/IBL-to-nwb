c3c13fad62d493923886e80a4e6d6596be2f3f7a
Saksham Sharda <saksham20.sharda@gmail.com>
2020-04-26 21:22:27 -0400

    update schema init

--- AlyxToNWB/schema/__init__.py
+++ AlyxToNWB/schema/__init__.py
@@ -1,13 +1,14 @@
 import json
 import os
-with open(os.path.join(os.getcwd(), 'metafile.schema.json')) as f:
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(dir_path, 'metafile.schema.json')) as f:
     metafile=json.load(f)
 
-with open(os.path.join(os.getcwd(), 'template_metafile.schema.json')) as f:
+with open(os.path.join(dir_path, 'template_metafile.schema.json')) as f:
     template_metafile=json.load(f)
 
-with open(os.path.join(os.getcwd(), 'dataset_format_list.json')) as f:
-    dataset_format_list=json.load(f)
-
-with open(os.path.join(os.getcwd(), 'dataset_details_list.json')) as f:
+with open(os.path.join(dir_path, 'dataset_description_list.json')) as f:
     dataset_details_list=json.load(f)
+
+with open(os.path.join(dir_path, 'alyx_subject_list.json')) as f:
+    alyx_subject_list=json.load(f)
