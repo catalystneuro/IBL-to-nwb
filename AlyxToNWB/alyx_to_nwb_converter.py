b3b20daa68d820392305efbad50a89d09e57bb73
Saksham Sharda <saksham20.sharda@gmail.com>
2020-09-14 22:56:03 -0500

    removed custom_loader

--- AlyxToNWB/alyx_to_nwb_converter.py
+++ AlyxToNWB/alyx_to_nwb_converter.py
@@ -456,7 +456,11 @@ class Alyx2NWBConverter(NWBConverter):
             return None
         if dataset_to_load not in self._loaded_datasets.keys():
             if len(dataset_to_load.split(',')) == 1:
-                loaded_dataset = self._custom_loader(self.eid, dataset_types=[dataset_to_load], dclass_output=True)
+                if 'ephysData.raw' in dataset_to_load and not 'ephysData.raw.meta' in self._loaded_datasets:
+                    meta = self.one_object.load(self.eid, dataset_types=['ephysData.raw.meta'])
+                    ch = self.one_object.load(self.eid, dataset_types=['ephysData.raw.ch'])
+                    self._loaded_datasets.update({'ephysData.raw.meta': meta, 'ephysData.raw.ch': ch})
+                loaded_dataset = self.one_object.load(self.eid, dataset_types=[dataset_to_load], dclass_output=True)
                 self._loaded_datasets.update({dataset_to_load: loaded_dataset})
                 return _load_as_array(loaded_dataset)
             else:# special case  when multiple datasets are involved
@@ -470,25 +474,6 @@ class Alyx2NWBConverter(NWBConverter):
                 return _load_as_array(loaded_dataset)
             else:
                 return loaded_dataset
-
-    def _custom_loader(self, eid, dataset_types=None, dclass_output=None):
-        datatypes = [['_iblmic_audioSpectrogram.frequencies','_iblmic_audioSpectrogram.power','_iblmic_audioSpectrogram.times_mic'],
-                     ['camera.dlc'],['_iblrig_Camera.raw','_iblrig_Camera.timestamps'],
-                     ['_iblqc_ephysTimeRms.rms','_iblqc_ephysTimeRms.timestamps',
-                      '_iblqc_ephysSpectralDensity.power','_iblqc_ephysSpectralDensity.freqs',
-                      'ephysData.raw.ap','ephysData.raw.lf','ephysData.raw.nidq','ephysData.raw.timestamps']]
-        eids=['383f300e-ff4e-479a-8967-a33ea51b436e','dfd8e7df-dc51-4589-b6ca-7baccfeb94b4',
-              'db4df448-e449-4a6f-a0e7-288711e7a75a','db4df448-e449-4a6f-a0e7-288711e7a75a']
-        eid_id = [j for j,i in enumerate(datatypes) if dataset_types[0] in i]
-        if eid_id:
-            eid_to_load=eids[eid_id[0]]
-        else:
-            eid_to_load=eid
-        if 'ephysData.raw' in dataset_types[0] and not 'ephysData.raw.meta' in self._loaded_datasets:
-            meta= self.one_object.load(eid_to_load, dataset_types=['ephysData.raw.meta'])
-            ch = self.one_object.load(eid_to_load, dataset_types=['ephysData.raw.ch'])
-            self._loaded_datasets.update({'ephysData.raw.meta': meta, 'ephysData.raw.ch':ch})
-        return self.one_object.load(eid_to_load,dataset_types=dataset_types, dclass_output=dclass_output)
 
     def _get_data(self, sub_metadata, probes=1):
         """
