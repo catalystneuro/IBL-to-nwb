88b1e270a7df4f0e7697331992228db50c71b9f7
Saksham Sharda <saksham20.sharda@gmail.com>
2020-10-17 17:45:50 +0530

    nwb to ibl conversion tests passed

--- tests/utils.py
+++ tests/utils.py
@@ -4,5 +4,9 @@ from pathlib import Path
 nonetype=type(None)
 
 _schema_loc = Path(__file__).parent/'metafile.schema.json'
-with open(_schema_loc,'r') as f:
-    json_schema = json.load(f)
+_iblsession_schema_loc = Path(__file__).parent/'ibl_sessions.schema.json'
+_iblsubject_schema_loc = Path(__file__).parent/'ibl_subject.schema.json'
+with open(_schema_loc,'r') as a, open(_iblsession_schema_loc,'r') as b, open(_iblsubject_schema_loc,'r') as c:
+    json_schema = json.load(a)
+    json_iblsession_schema = json.load(b)
+    json_iblsubject_schema = json.load(c)
