a35b44c4afe57d59461131fd185b7eeb58383394
Saksham Sharda <saksham20.sharda@gmail.com>
2020-10-22 18:40:30 +0530

    folder name change, update setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='ibl_to_nwb',
-    version='0.1.0',
+    version='0.1.2',
     description='Tools to convert IBL data to NWB format',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -19,17 +19,16 @@ setup(
     author_email='saksham20.sharda@gmail.com',
     url='https://github.com/catalystneuro/IBL-to-nwb',
     keywords=['nwb', 'ibl'],
-    packages=find_packages(),
+    packages=['ibl_to_nwb'],
     package_data={},
-    include_package_data=True,
+    include_package_data=False,
     install_requires=[
-        'pynwb', 'numpy', 'nwbwidgets', 'pandas', 'pyarrow', 'scipy >= 1.4.1',
-        'tqdm', 'tzlocal', 'datajoint', 'mtscomp', 'ndx_ibl_metadata',
-        'git+https://github.com/catalystneuro/nwb-conversion-tools.git@fb9703f8e86072f04356883975e5dfffa773913e#egg=nwb-conversion-tools',
+        'pynwb', 'numpy', 'pandas', 'scipy >= 1.4.1',
+        'tqdm', 'tzlocal', 'ndx_ibl_metadata',
         'ndx-spectrum==0.2.2',
-        'ndx-icephys-meta==0.1.0',
         'ibllib',
         'lazy_ops',
-        'pyrsistent==0.16.0'
-    ]
+    ],
+    dependency_links=['git+https://github.com/catalystneuro/nwb-conversion-tools.git@fb9703f8e86072f04356883975e5dfffa773913e#egg=nwb-conversion-tools'],
+    python_requires='>=3.6'
 )
