ba10b5e6538a8af2ac5dd2cb347edc946fd469a1
weiglszonja <wszonja@gmail.com>
2023-01-17 14:01:54 +0100

    add helper tool for retrieving the number of probes for a session

--- src/ibl_to_nwb/tools/one_api/helpers.py
+++ src/ibl_to_nwb/tools/one_api/helpers.py
@@ -25,3 +25,11 @@ def session_to_probe_id_mapping(one: ONE):
             session_to_probe_id[probe_entry["session"]] = [probe_entry["id"]]
 
     return session_to_probe_id
+
+
+def get_num_probes(one: ONE, probe_id: str):
+    query_str = "*probe*ap.cbin"
+    session_id, _ = one.pid2eid(pid=probe_id)
+
+    num_probes = len(one.list_datasets(session_id, query_str))
+    return num_probes
