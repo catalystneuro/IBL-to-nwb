1129a5d4860111e2b5f3dc626c7cef49f0b2e487
Cody Baker <cbaker9@nd.edu>
2023-01-17 16:23:22 -0500

    add basic structure

---
+++ ibl_to_nwb/repeated_site/datainterfaces/streamingiblinterface.py
@@ -0,0 +1,22 @@
+from neuroconv.datainterfaces.ecephys.baserecordingextractorinterface import BaseRecordingExtractorInterface
+from neuroconv.datainterfaces.ecephys.baselfpextractorinterface import BaseLFPExtractorInterface
+
+class StreamingIblRecordingInterface(BaseRecordingExtractorInterface):
+    ExtractorName = "StreamingIblExtractor"
+
+    @classmethod
+    def get_stream_names(cls):
+        pass # TODO: constrain to ap streams
+
+    def get_metadata(self):
+        pass # TODO: add descriptions for all those custom properties
+
+class StreamingIblLfpInterface(BaseLFPExtractorInterface):
+    ExtractorName = "StreamingIblExtractor"
+
+    @classmethod
+    def get_stream_names(cls):
+        pass # TODO: constrain to lf streams
+
+    def get_metadata():
+        pass # TODO: add descriptions for all those custom properties
