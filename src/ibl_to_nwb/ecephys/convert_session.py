b3fdc22b3379b66f72019be89298cf70371beba2
weiglszonja <wszonja@gmail.com>
2023-01-17 14:02:20 +0100

    update convert_session.py

--- src/ibl_to_nwb/ecephys/convert_session.py
+++ src/ibl_to_nwb/ecephys/convert_session.py
@@ -1,16 +1,21 @@
-from src.ibl_to_nwb.tools.ecephys.spikeglx_to_nwb import write_recording
+from ibl_to_nwb.tools.ecephys.spikeglx_to_nwb import write_recording
+from ibl_to_nwb.tools.ecephys.spikeglx_to_nwb import IBL_CONFIG
+
+
+cache_path = "/Volumes/t7-ssd/ibl_cache/"
 
 # This is a unique identifier for an experimental session, required for streaming the data from Alyx database
-pid = "da8dfec1-d265-44e8-84ce-6ae9c109b8bd"
+session_id = "0f77ca5d-73c2-45bd-aa4c-4c5ed275dbde"
 
-nwbfile_path = f"/Volumes/t7-ssd/ibl_cache/{pid}_ibl_ecephys_stub.nwb"
+# Temporarily use session id in the name
+nwbfile_path = f"/Volumes/t7-ssd/ibl_cache/{session_id}_ibl_ecephys_stub.nwb"
 
 session_start_time = (
     "2020-09-21T18:58:14"  # TODO: access this from ONE api and other metadata
 )
 write_recording(
     nwbfile_path=nwbfile_path,
-    pid=pid,
+    session_id=session_id,
     metadata=dict(NWBFile=dict(session_start_time=session_start_time)),
 )
 
