d0bab2c33ea7076dea09ea9bc94800edf4ed5223
Cody Baker <cbaker9@nd.edu>
2023-01-18 12:55:20 -0500

    setup experiment-specific metadata

--- ibl_to_nwb/updated_conversion/brainwide_map/brainwidemapconverter.py
+++ ibl_to_nwb/updated_conversion/brainwide_map/brainwidemapconverter.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 from one.api import ONE
+from neuroconv.utils import load_dict_from_file
 
 from ..iblconverter import IblConverter
 
@@ -6,6 +9,12 @@ from ..iblconverter import IblConverter
 class BrainwideMapConverter(IblConverter):
 
     def get_metadata(self):
+        metadata = super().get_metadata()
+
         one = ONE(base_url="https://openalyx.internationalbrainlab.org", password="international", silent=True)
 
         # TODO: fetch session and subject-level metadata, including comments/notes
+        experiment_metadata = load_dict_from_file(file_path=Path(__file__) / "experiment_metadata.yml")
+
+        metadata["NWBFile"]["experiment_description"] = experiment_metadata["experiment_description"]
+        metadata["NWBFile"]["related_publications"] = experiment_metadata["related_publications"]
