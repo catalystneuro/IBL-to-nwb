ceef68eebe47fce3fb7bc7dc889c0b3c6734a60f
Cody Baker <cbaker9@nd.edu>
2023-01-26 15:12:47 -0500

    added pose estimation series for dlc datta

--- ibl_to_nwb/updated_conversion/brainwide_map/convert_brainwide_map.py
+++ ibl_to_nwb/updated_conversion/brainwide_map/convert_brainwide_map.py
@@ -1,18 +1,21 @@
 from pathlib import Path
 
 from one.api import ONE
+from pydantic import DirectoryPath
 
 from ibl_to_nwb.updated_conversions import StreamingIblRecordingInterface, StreamingIblLfpInterface
 from ibl_to_nwb.updated_conversions.brainwidemap import BrainwideMapConverter
+from ibl_to_nwb.updated_conversions.data_interfaces import AlfDlcInterface
 
 one = ONE(base_url="https://openalyx.internationalbrainlab.org", password="international", silent=True)
 
 sessions = one.alyx.rest(url="sessions", action="list", tag="2022_Q2_IBL_et_al_RepeatedSite")
 
 
-def convert_session(session: str, nwbfile_path: str):
+def convert_session(session: str, base_path: DirectoryPath):
     # Download behavior and spike sorted data for this session
     session_path = base_path / session
+    cache_folder = base_path / session / "cache"
 
     # Get stream names from SI
     ap_stream_names = StreamingIblRecordingInterface.get_stream_names(session=session)
@@ -24,7 +27,12 @@ def convert_session(session: str, nwbfile_path: str):
         data_interfaces.append(StreamingIblRecordingInterface(session=session, stream_name=stream_name))
     for stream_name in lf_stream_names:
         data_interfaces.append(StreamingIblLfpInterface(session=session, stream_name=stream_name))
-    # TODO: initialize behavior and spike sorting interfaces
+    # TODO: initialize behavior and spike sorting
+
+    for camera_name in ["leftCamera", "rightCamera", "bodyCamera"]:
+        camera_files = one.list_datasets(eid=session, filename=f"*{camera_name}.dlc*")
+        if any("dlc" in x for x in camera_files):
+            data_interfaces.append(AlfDlcInterface(session=session, cache_folder=cache_folder, camera_name=camera_name))
 
     # Run conversion
     nwbfile_path = session_path / f"{session}.nwb"
