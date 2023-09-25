37f50684593b893e28f262b2947d3aaa95da8211
CodyCBakerPhD <cody.c.baker.phd@gmail.com>
2023-09-25 03:27:57 +0000

    make video inclusion controllable with pose

--- ibl_to_nwb/updated_conversion/datainterfaces/iblposeestimationinterface.py
+++ ibl_to_nwb/updated_conversion/datainterfaces/iblposeestimationinterface.py
@@ -10,11 +10,12 @@ from pynwb.image import ImageSeries
 
 
 class IblPoseEstimationInterface(BaseDataInterface):
-    def __init__(self, one: ONE, session: str, camera_name: str, include_video: bool):
+    def __init__(self, one: ONE, session: str, camera_name: str, include_video: bool, include_pose: bool):
         self.one = one
         self.session = session
         self.camera_name = camera_name
         self.include_video = include_video
+        self.include_pose = include_pose
 
     def get_original_timestamps(self):
         pass
@@ -52,31 +53,36 @@ class IblPoseEstimationInterface(BaseDataInterface):
         camera_name_snake_case = f"{left_right_or_body}_camera"
         reused_timestamps = None
         all_pose_estimation_series = list()
-        for body_part in body_parts:
-            body_part_data = np.empty(shape=(number_of_frames, 2))
-            body_part_data[:, 0] = dlc_data[f"{body_part}_x"]
-            body_part_data[:, 1] = dlc_data[f"{body_part}_y"]
+        if self.include_pose:
+            for body_part in body_parts:
+                body_part_data = np.empty(shape=(number_of_frames, 2))
+                body_part_data[:, 0] = dlc_data[f"{body_part}_x"]
+                body_part_data[:, 1] = dlc_data[f"{body_part}_y"]
 
-            pose_estimation_series = PoseEstimationSeries(
-                name=body_part,
-                description=f"Marker placed on or around, labeled '{body_part}'.",
-                data=H5DataIO(body_part_data, compression=True),
-                unit="px",
-                reference_frame="(0,0) corresponds to the upper left corner when using width by height convention.",
-                timestamps=reused_timestamps or H5DataIO(timestamps, compression=True),
-                confidence=np.array(dlc_data[f"{body_part}_likelihood"]),
+                pose_estimation_series = PoseEstimationSeries(
+                    name=body_part,
+                    description=f"Marker placed on or around, labeled '{body_part}'.",
+                    data=H5DataIO(body_part_data, compression=True),
+                    unit="px",
+                    reference_frame="(0,0) corresponds to the upper left corner when using width by height convention.",
+                    timestamps=reused_timestamps or H5DataIO(timestamps, compression=True),
+                    confidence=np.array(dlc_data[f"{body_part}_likelihood"]),
+                )
+                all_pose_estimation_series.append(pose_estimation_series)
+
+                reused_timestamps = all_pose_estimation_series[0]  # trick for linking timestamps across series
+
+            pose_estimation_kwargs = dict(
+                name=f"PoseEstimation{left_right_or_body.capitalize()}Camera",
+                pose_estimation_series=all_pose_estimation_series,
+                description="Estimated positions of body parts using DeepLabCut.",
+                source_software="DeepLabCut",
+                nodes=body_parts,
             )
-            all_pose_estimation_series.append(pose_estimation_series)
-
-            reused_timestamps = all_pose_estimation_series[0]  # trick for linking timestamps across series
-
-        pose_estimation_kwargs = dict(
-            name=f"PoseEstimation{left_right_or_body.capitalize()}Camera",
-            pose_estimation_series=all_pose_estimation_series,
-            description="Estimated positions of body parts using DeepLabCut.",
-            source_software="DeepLabCut",
-            nodes=body_parts,
-        )
+            pose_estimation_container = PoseEstimation(**pose_estimation_kwargs)
+            behavior_module = get_module(nwbfile=nwbfile, name="behavior", description="Processed behavioral data.")
+            behavior_module.add(pose_estimation_container)
+        
         if self.include_video and self.one.list_datasets(eid=self.session, filename=f"raw_video_data/*{self.camera_name}*"):
             original_video_file = self.one.load_dataset(
                 id=self.session, dataset=f"raw_video_data/*{self.camera_name}*", download_only=True
@@ -91,7 +97,3 @@ class IblPoseEstimationInterface(BaseDataInterface):
             )
             # pose_estimation_kwargs.update(original_videos_series=[image_series])  # For future version of ndx-pose
             nwbfile.add_acquisition(image_series)
-
-        pose_estimation_container = PoseEstimation(**pose_estimation_kwargs)
-        behavior_module = get_module(nwbfile=nwbfile, name="behavior", description="Processed behavioral data.")
-        behavior_module.add(pose_estimation_container)
