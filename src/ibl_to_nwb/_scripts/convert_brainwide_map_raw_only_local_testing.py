a1614735e4f81de2923aa2a6804fc0909cc6f8d1
grg2rsr <grg2rsr@gmail.com>
2024-10-25 15:09:30 +0100

    for local testing

---
+++ src/ibl_to_nwb/_scripts/convert_brainwide_map_raw_only_local_testing.py
@@ -0,0 +1,78 @@
+from pathlib import Path
+
+from one.api import ONE
+
+from ibl_to_nwb.converters import BrainwideMapConverter, IblSpikeGlxConverter
+from ibl_to_nwb.datainterfaces import RawVideoInterface
+
+# session_id = "d32876dd-8303-4720-8e7e-20678dc2fd71"
+session_id = "caa5dddc-9290-4e27-9f5e-575ba3598614"  # a BWM session with dual probe
+data_folder = Path(
+    "/media/georg/openlab/Downloads/ONE/openalyx.internationalbrainlab.org/steinmetzlab/Subjects/NR_0031/2023-07-14/001"
+)
+spikeglx_source_folder_path = data_folder / "raw_ephys_data"
+
+# Specify the revision of the pose estimation data
+# Setting to 'None' will use whatever the latest released revision is
+revision = None
+
+# base_path = Path("E:/IBL")
+base_path = Path.home() / "ibl_scratch"  # local directory
+base_path.mkdir(exist_ok=True)
+nwbfiles_folder_path = base_path / "nwbfiles"
+nwbfiles_folder_path.mkdir(exist_ok=True)
+
+# Initialize IBL (ONE) client to download processed data for this session
+one_cache_folder_path = base_path / "cache"
+ibl_client = ONE(
+    base_url="https://openalyx.internationalbrainlab.org",
+    password="international",
+    silent=True,
+    cache_dir=one_cache_folder_path,
+)
+
+# Specify the path to the SpikeGLX files on the server but use ONE API for timestamps
+data_interfaces = []
+
+# spikeglx_source_folder_path = Path("D:/example_data/ephy_testing_data/spikeglx/Noise4Sam_g0")
+spikeglx_subconverter = IblSpikeGlxConverter(folder_path=spikeglx_source_folder_path, one=ibl_client, eid=session_id)
+data_interfaces.append(spikeglx_subconverter)
+
+# # Raw video takes some special handling
+# metadata_retrieval = BrainwideMapConverter(one=ibl_client, session=session_id, data_interfaces=[], verbose=False)
+# subject_id = metadata_retrieval.get_metadata()["Subject"]["subject_id"]
+
+# pose_estimation_files = ibl_client.list_datasets(eid=session_id, filename="*.dlc*")
+# for pose_estimation_file in pose_estimation_files:
+#     camera_name = pose_estimation_file.replace("alf/_ibl_", "").replace(".dlc.pqt", "")
+
+#     video_interface = RawVideoInterface(
+#         nwbfiles_folder_path=nwbfiles_folder_path,
+#         subject_id=subject_id,
+#         one=ibl_client,
+#         session=session_id,
+#         camera_name=camera_name,
+#     )
+#     data_interfaces.append(video_interface)
+
+# Run conversion
+session_converter = BrainwideMapConverter(
+    one=ibl_client, session=session_id, data_interfaces=data_interfaces, verbose=False
+)
+
+metadata = session_converter.get_metadata()
+metadata["NWBFile"]["session_id"] = metadata["NWBFile"]["session_id"]
+subject_id = metadata["Subject"]["subject_id"]
+
+subject_folder_path = nwbfiles_folder_path / f"sub-{subject_id}"
+subject_folder_path.mkdir(exist_ok=True)
+nwbfile_path = subject_folder_path / f"sub-{subject_id}_ses-{session_id}_desc-raw_ecephys+image.nwb"
+
+session_converter.run_conversion(
+    nwbfile_path=nwbfile_path,
+    metadata=metadata,
+    overwrite=True,
+)
+
+# TODO: add some kind of raw-specific check
+# check_written_nwbfile_for_consistency(one=ibl_client, nwbfile_path=nwbfile_path)
--- src/ibl_to_nwb/converters/_ibl_spikeglx_converter.py
+++ src/ibl_to_nwb/converters/_ibl_spikeglx_converter.py
@@ -2,39 +2,42 @@ from neuroconv.converters import SpikeGLXConverterPipe
 from one.api import ONE
 from pydantic import DirectoryPath
 from pynwb import NWBFile
+import numpy as np
+from brainbox.io.one import SpikeSortingLoader, EphysSessionLoader
 
 
 class IblSpikeGlxConverter(SpikeGLXConverterPipe):
-
-    def __init__(self, folder_path: DirectoryPath, one: ONE) -> None:
+    def __init__(self, folder_path: DirectoryPath, one: ONE, eid: str) -> None:
         super().__init__(folder_path=folder_path)
         self.one = one
+        self.eid = eid  # probably should better name this session_id ?
 
     def temporally_align_data_interfaces(self) -> None:
         """Align the raw data timestamps to the other data streams using the ONE API."""
         # This is the syntax for aligning the raw timestamps; I cannot test this without the actual data as stored
         # on your end, so please work with Heberto if there are any problems after uncommenting
-        # probe_to_imec_map = {
-        #     "probe00": 0,
-        #     "probe01": 1,
-        # }
-        #
-        # ephys_session_loader = EphysSessionLoader(one=self.one, eid=session_id)
-        # probes = ephys_session_loader.probes
-        # for probe_name, pid in ephys_session_loader.probes.items():
-        #     spike_sorting_loader = SpikeSortingLoader(pid=pid, one=ibl_client)
-        #
-        #     probe_index = probe_to_imec_map[probe_name]
-        #     for band in ["ap", "lf"]:
-        #         recording_interface = next(
-        #             interface
-        #             for interface in self.data_interface_objects
-        #             if f"imec{probe_index}.{band}" in interface.source_data["file_path"]
-        #         )
-        #
-        #         band_info = spike_sorting_loader.raw_electrophysiology(band=band, stream=True)
-        #         aligned_timestamps = spike_sorting_loader.samples2times(numpy.arange(0, band_info.ns), direction='forward')
-        #         recording_interface.set_aligned_timestamps(aligned_timestamps=aligned_timestamps)
+        probe_to_imec_map = {
+            "probe00": 0,
+            "probe01": 1,
+        }
+
+        ephys_session_loader = EphysSessionLoader(one=self.one, eid=self.eid)
+        probes = ephys_session_loader.probes
+        for probe_name, pid in ephys_session_loader.probes.items():
+            spike_sorting_loader = SpikeSortingLoader(pid=pid, one=self.one)
+
+            probe_index = probe_to_imec_map[probe_name]
+            for band in ["ap", "lf"]:
+                recording_interface = self.data_interface_objects[f"imec{probe_index}.{band}"]
+                # recording_interface = next(
+                #     interface
+                #     for interface in self.data_interface_objects
+                #     if f"imec{probe_index}.{band}" in interface.source_data["file_path"]
+                # )
+
+                band_info = spike_sorting_loader.raw_electrophysiology(band=band, stream=True)
+                aligned_timestamps = spike_sorting_loader.samples2times(np.arange(0, band_info.ns), direction="forward")
+                recording_interface.set_aligned_timestamps(aligned_timestamps=aligned_timestamps)
         pass
 
     def add_to_nwbfile(self, nwbfile: NWBFile, metadata) -> None:
