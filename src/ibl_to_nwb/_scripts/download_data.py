a9afd443aa5329913768d77fce2033154a30ad6c
Georg Raiser <grg2rsr@gmail.com>
2025-06-11 21:28:29 +0100

    download script for heberto

--- src/ibl_to_nwb/_scripts/download_data.py
+++ src/ibl_to_nwb/_scripts/download_data.py
@@ -5,8 +5,8 @@ import sys
 
 # %% session selection
 # eid = "caa5dddc-9290-4e27-9f5e-575ba3598614"  # a BWM eid with dual probe
-eid = "09394481-8dd2-4d5c-9327-f2753ede92d7"  # the spike timestamps issue for Heberto
-# eid = "6713a4a7-faed-4df2-acab-ee4e63326f8d" # the LF timestamps issue
+# eid = "09394481-8dd2-4d5c-9327-f2753ede92d7"  # the spike timestamps issue for Heberto
+eid = "6713a4a7-faed-4df2-acab-ee4e63326f8d"  # the LF timestamps issue
 
 # %% path setup
 base_path = Path.home() / "ibl_scratch"  # local directory
