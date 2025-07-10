73ed46068aa755c355c26acf1ccaf6dbb5a91e4f
Georg Raiser <grg2rsr@gmail.com>
2025-07-10 12:36:35 +0100

    adding all bwm QC to fixtures for dataset selection

--- src/ibl_to_nwb/_scripts/update_fixtures.py
+++ src/ibl_to_nwb/_scripts/update_fixtures.py
@@ -2,24 +2,39 @@
 
 # download bwm units table
 from pathlib import Path
-
 import pandas as pd
 from brainwidemap import bwm_query, download_aggregate_tables
 from one.api import ONE
+import json
+from tqdm import tqdm
 
 one = ONE()
 
 # saving bwm_units_df.pqt
 # bwm_units_df = bwm_units(one, min_qc=0)
+print("bwm units table")
 bwm_units_df = pd.read_parquet(download_aggregate_tables(one=one))
 outpath = Path(__file__).parent.parent / "fixtures" / "bwm_units_df.pqt"
 outpath.parent.mkdir(parents=True, exist_ok=True)
+print(f"saving {outpath}")
 bwm_units_df.to_parquet(outpath)
-print(bwm_units_df.shape[0])
 
 # saving bwm_df
 # TODO proper freeze, also return_details and freeze together doesn't work
+print("bwm sessions table")
 bwm_df = bwm_query(freeze="2023_12_bwm_release", one=one, return_details=True)
 outpath = Path(__file__).parent.parent / "fixtures" / "bwm_df.pqt"
 outpath.parent.mkdir(parents=True, exist_ok=True)
+print(f"saving {outpath}")
 bwm_df.to_parquet(outpath)
+
+# saving the entire qc from alyx
+print("getting qc")
+bwm_qc = {}
+for eid in tqdm(bwm_df["eid"].unique()):
+    bwm_qc[eid] = one.alyx.rest("sessions", "read", eid)["extended_qc"]
+outpath = Path(__file__).parent.parent / "fixtures" / "bwm_qc.json"
+
+print(f"saving {outpath}")
+with open(outpath, "w") as json_file:
+    json.dump(bwm_qc, json_file)
--- src/ibl_to_nwb/fixtures/load_fixtures.py
+++ src/ibl_to_nwb/fixtures/load_fixtures.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import json
 import pandas as pd
 
 
@@ -11,3 +11,9 @@ def load_bwm_df():
 def load_bwm_units_df():
     path = Path(__file__).parent / "bwm_units_df.pqt"
     return pd.read_parquet(path)
+
+
+def load_bwm_qc():
+    path = Path(__file__).parent / "bwm_qc.json"
+    with open(path, "r") as fH:
+        return json.load(fH)
